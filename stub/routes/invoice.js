04623aaa70d0cec7643bad61d04569b4c8707bc8
Alejandro <alexseik@hotmail.com>
2014-11-26 19:50:49 +0100

    Invoice Rest api finished

--- stub/routes/invoice.js
+++ stub/routes/invoice.js
@@ -17,3 +17,40 @@ var INVOICE  =[{
     total: 30.5,
     discount: 0.10
 }];
+
+
+exports.fetchInvoices = function (req, res) {
+    return res.json(200, INVOICE);
+};
+
+exports.fetchInvoice = function (req, res) {
+    var id = req.params.id;
+    if (id < 1 || id > INVOICE.length) return res.json(422, { error: 'The client does not exist.' });
+    return res.json(200, INVOICE[id-1]);
+};
+
+
+exports.saveInvoice = function (req,res){
+    var invoice = req.body;
+    if (invoice.id != "") res.json(422, { error: 'The invoice already exist.' });
+    if (invoice.tax == undefined ||
+        (invoice.client == undefined &&
+        (invoice.total == undefined))) res.json(422, { error: 'The client does not contain data.' });
+
+    invoice.id = INVOICE.length;
+    return res.json(201, invoice);
+};
+
+exports.updateInvoice = function (req,res){
+    var invoice = req.body;
+    var id = req.params.id;
+    if (id != invoice.id) res.json(400, { error: 'The client is not the same.' });
+    if (id < 1 || id > INVOICE.length) res.json(404, { error: 'The client id does not exist.' });
+    return exports.saveInvoice(req,res);
+};
+
+exports.deleteInvoice = function (req, res){
+    var id = req.params.id;
+    if (id < 1 || id > INVOICE.length) res.json(404, { error: 'The client id does not exist.' });
+    return res.json(200)
+};
