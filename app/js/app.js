38ca7ba23cc4b8c4ba7459ec52ea9ebe5ae29cab
Alejandro <alexseik@hotmail.com>
2014-12-03 21:37:35 +0100

    Include angular bootstrap and pagination in list tickets

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("app", ['ngRoute','xeditable','angularUtils.directives.dirPagination']);
+var app = angular.module("app", ['ngRoute','xeditable','ui.bootstrap']);
 
 app.config(function ($routeProvider) {
     "use strict";
--- app/js/controller/ticketListController.js
+++ app/js/controller/ticketListController.js
@@ -2,4 +2,13 @@
 angular.module("app").controller("TicketListController", function ($scope,Tickets) {
     "use strict";
     $scope.ticketList = new Tickets();
+
+    $scope.totalItems = $scope.ticketList.length;
+    $scope.currentPage = 1;
+    $scope.maxSize = 5;
+
+    $scope.pageChanged = function() {
+
+
+    };
 });
